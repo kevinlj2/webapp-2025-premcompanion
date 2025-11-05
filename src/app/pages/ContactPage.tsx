c5706f7e9c32f610d77cf87aacd79cb30ecb6c12
kevinlj2 <kevlok4@gmail.com>
2025-11-05 11:24:57 +0100

    Moved up to /pages

---
+++ src/app/pages/ContactPage.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>This is where the contact information will be displayed.</p>
+    </div>
+  );
+}
