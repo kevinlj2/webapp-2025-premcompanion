9da9506035dac02058b6515308612da6dde1de89
kevinlj2 <kevlok4@gmail.com>
2025-11-05 11:25:11 +0100

    Removed from components

--- src/app/pages/components/TransHisPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function TransHisPage() {
-  return (
-    <div>
-      <h2>Transaction History Page</h2>
-      <p>This is where the transaction history will be displayed.</p>
-    </div>
-  );
-}
