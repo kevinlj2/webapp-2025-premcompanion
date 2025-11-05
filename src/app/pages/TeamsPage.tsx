5fc0791ccf689ef0dd9b0dd5012043ebd6038b83
kevinlj2 <kevlok4@gmail.com>
2025-11-05 11:24:45 +0100

    Created TeamsPage
    Page currently shows all active premier league teams

---
+++ src/app/pages/TeamsPage.tsx
@@ -0,0 +1,10 @@
+import Teams from "./components/Teams";
+import Layout from "./components/Layout";
+
+export function TeamsPage() {
+  return (
+    <Layout>
+      <Teams />
+    </Layout>
+  );
+}
