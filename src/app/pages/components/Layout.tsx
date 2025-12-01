040970359beec02bb4d6d5fc6b24b74f1f7d491c
kevinlj2 <kevlok4@gmail.com>
2025-12-01 14:18:27 +0100

    Updated layout
    Added footer

--- src/app/pages/components/Layout.tsx
+++ src/app/pages/components/Layout.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { LayoutProps } from "rwsdk/router";
 import Nav from "./Nav";
+import PremFooter from "./PremFooter";
 
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex min-h-screen bg-gray-100">
       <Nav />
 
-      <main className="flex-1 bg-gray-100 p-6 overflow-y-auto">
-        <div className="max-w-5xl mx-auto">{children}</div>
-      </main>
+      <div className="flex-1 flex flex-col">
+        <main className="flex-1 p-6 overflow-y-auto bg-gray-100">
+          <div className="max-w-5xl mx-auto">{children}</div>
+        </main>
+
+        <PremFooter />
+      </div>
     </div>
   );
 }
