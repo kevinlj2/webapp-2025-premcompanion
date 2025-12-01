47a9a6714a3bbfa5938ca781ddb15ca2805486ec
kevinlj2 <kevlok4@gmail.com>
2025-12-01 14:22:14 +0100

    Created registerpage
    Route to worker.tsx

---
+++ src/app/pages/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import Layout from "./components/Layout";
+import Register from "./components/Register";
+
+export function RegisterPage() {
+  return (
+    <Layout>
+      <Register />
+    </Layout>
+  );
+}
